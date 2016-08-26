06aa092a3735a0022958bc9dead0ff5116ca0fc9
littlezim <littlezim18@gmail.com>
2016-08-26 11:24:54 -0700

    Files For VSTDA Angular

--- src/js/home.controller.js
+++ src/js/home.controller.js
@@ -1,12 +1,31 @@
 //Create controller for module
 angular.module('app').controller('HomeController', function($scope) {
-//create array for text	
+
+
+	//create array for text	
 	$scope.toDos = [];
-//get and store array information from user input and selector
+
+
+	//get and store array information from user input and selector
 	$scope.addToDo = function() {
+		
 		$scope.toDos.push({
+			
 			text : $scope.text,
+			
 			priority : parseInt($scope.priority)
+
 		});
+	};
+
+	//Delete array information
+	$scope.deleteDo = function(text) {
+
+		//Find index of text for the array
+		var indexOfDo = $scope.toDos.indexOf(text);
+		
+		//Splice to position of text input and remove that text
+		$scope.toDos.splice(indexOfDo, 1);
+
 	};
 });
